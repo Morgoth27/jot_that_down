e4920952d62c85ad7e63d1b554599a2a2bcf2ba9
Tiffany <leopardmitai@gmail.com>
2022-02-06 23:49:55 -0800

    Adding routing, preparing to reoganize some files in directory as well

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -1,20 +1,28 @@
-const uuid = require('uuid');
 const express = require('express');
-const { append } = require('vary');
-const termData = ('')
+// const termData = ('./terms.json')
 const path = require('path');
-const { ppid } = require('process');
-const PORT = 3001;
 
-app.use( express.static('public'));
+const PORT = 3001;
+const app = express();
+
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
-{}
+{
+    res.sendFile(path.join( __dirname, 'public/notes.html'));
+}
 );
 
-app.get('/api/notes', (req, res)=>
-{}
-);
+// app.get('/api/notes', (req, res)=>
+// {
+//     res.json();
+// }
+// );
 
-append.listen(PORT, () =>
-conesole.log(`Example app listening at http://localhost:${PORT}`));
+app.post('/api/notes', (req, res) => 
+{
+    res.json('testing')
+})
+
+app.listen(PORT, () =>
+console.log(`Example app listening at http://localhost:${PORT}`));
