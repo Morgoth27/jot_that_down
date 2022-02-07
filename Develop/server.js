17836150ba7f19c33f86fc8130600b73fb9329ea
Tiffany <leopardmitai@gmail.com>
2022-02-07 02:54:36 -0800

    added some code, trying to troubleshoot why it wont render notes on page. i think its a directory issue.

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const notes = ('/Develop/db/db.json')
+const notes = require('./db/db.json');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
@@ -11,17 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) =>
-{
+app.get('/', (req, res) => {
     res.sendFile(path.join( __dirname, 'public/notes.html'));
-}
-);
+});
 
-// app.get('/api/notes', (req, res)=>
-// {
-//     res.json();
-// }
-// );
+app.get('api/notes', (req, res) => {
+    res.json('Request received to get notes.');
+});
 
 app.post('/api/notes', (req, res) => {
     const {title, text} = req.body;
@@ -29,7 +26,8 @@ app.post('/api/notes', (req, res) => {
     if (title && text) {
         const newNote = {
             title,
-            text
+            text,
+            review_id: uuid(),
         };
 
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -40,16 +38,18 @@ app.post('/api/notes', (req, res) => {
 
                 parsedNotes.push(newNote);
 
-                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 2),
-                (writeErr) =>
-                writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully added new note!')
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null),
+                // (writeErr) =>
+                // writeErr
+                // ? console.error(writeErr)
+                console.info('Successfully added new note!')
                 );
             }
         });
+
+
         const response = {
-            status: 'Success!',
+            status: 'success',
             body: newNote,
         };
 
